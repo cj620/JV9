16bb9b21aadee80aa92933ae904880427cde8c57
勿忘 <208760845@qq.com>
2023-07-14 09:21:46 +0800

    修改销售报价

--- src/views/workModule/sale/saleQuote/Add/formConfig.js
+++ src/views/workModule/sale/saleQuote/Add/formConfig.js
@@ -1,13 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 13:26:31
- * @LastEditTime: 2022-01-14 09:51:52
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-07-14 08:59:47
+ * @LastEditors: 勿忘 208760845@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \V9_Dev\src\views\workModule\sale\saleOrder\components\formConfig.js
  */
 import { getAll } from "@/api/basicApi/systemSettings/currency";
 import { getAllSalesCustomer } from "@/api/workApi/sale/customer";
+import {
+  getProjectQuery,
+} from "@/api/workApi/project/projectManage";
 export const formSchema = [
   // {
   //   // 报价编号
@@ -48,6 +51,7 @@ export const formSchema = [
     prop: "Project",
     cpn: "FormInput",
     label: i18n.t("menu.Pm_Project"),
+    custom: true,
   },
   {
     // 名称
