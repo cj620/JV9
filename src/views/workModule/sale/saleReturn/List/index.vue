49046de2a5bdf300c5ae7c331842851bcb29407d
陈祎初 <18530100115@163.com>
2023-07-26 10:25:23 +0800

    feat: 销售退货复制功能

--- src/views/workModule/sale/saleReturn/List/index.vue
+++ src/views/workModule/sale/saleReturn/List/index.vue
@@ -3,53 +3,13 @@
   <PageWrapper :footer="false">
     <!-- 表格 -->
     <JvTable ref="BillTable" :table-obj="tableObj">
-      <template #State="{ record }">
-        <BillStateTags :state="record"></BillStateTags>
-      </template>
-
-      <template #operation="{ row }">
-        <TableAction
-          :actions="[
-            {
-              label: $t('Generality.Ge_Edit'),
-              confirm: edit.bind(null, row),
-              disabled: getActionState(row.State, 'edit'),
-            },
-            {
-              label: $t('Generality.Ge_Delete'),
-              disabled: getActionState(row.State, 'edit'),
-              popConfirm: {
-                title: $t('Generality.Ge_DeleteConfirm'),
-                confirm: deleteOrder.bind(null, [row.BillId]),
-              },
-            },
-          ]"
-        />
-      </template>
-      <Action
-        size="mini"
-        slot="btn-list"
-        :actions="[
-          {
-            label: $t('Generality.Ge_New'),
-
-            confirm: add.bind(),
-          },
-          {
-            label: $t('Generality.Ge_Delete'),
-            disabled: canIsDel,
-            popConfirm: {
-              title: $t('Generality.Ge_DeleteConfirm'),
-              confirm: del.bind(),
-            },
-          },
-          {
-            label: $t('project.Pro_CheckDetails'),
-            confirm: toDetailsList,
-          },
-        ]"
-      >
+      <!-- 表格操作行 -->
+      <Action size="mini" slot="btn-list" :actions="getListTableBtnModel">
       </Action>
+      <!-- 表格操作列按钮 -->
+      <template #operation="{ row }">
+        <TableAction :actions="getListTableColBtnModel(row)" />
+      </template>
     </JvTable>
   </PageWrapper>
 </template>
@@ -57,6 +17,10 @@
 import { Table } from "./config";
 import { stateEnum } from "@/enum/workModule";
 import BillStateTags from "@/components/WorkModule/BillStateTags";
+import {
+  listTableBtnModel,
+  listTableColBtnModel,
+} from "@/jv_doc/utils/system/pagePlugin";
 export default {
   name: "Sa_SaleReturn",
   components: {
@@ -70,6 +34,10 @@ export default {
         type: "",
         data: "",
       },
+      // 编辑路由
+      EditRoute: "Sa_SaleReturn_Edit",
+      // 新增路由
+      AddRoute: "Sa_SaleReturn_Add",
       IsState: false,
       dialogFormVisible: false,
       chooseList: [],
@@ -80,18 +48,20 @@ export default {
     this.tableObj.getData();
   },
   computed: {
-    // 是否可以批量删除
-    canIsDel() {
-      let { datas } = this.tableObj.selectData;
-      if (datas.length === 0) return true;
-      return datas.some((item) => {
-        return !["Rejected", "Unsubmitted"].includes(item.State);
-      });
+    // 表格操作模块
+    getListTableBtnModel() {
+      return [
+        ...listTableBtnModel(this),
+        {
+          label: i18n.t("project.Pro_CheckDetails"),
+          confirm: this.toDetailsList,
+        },
+      ];
     },
-    // 获取按钮状态
-    getActionState() {
-      return (state, type) => {
-        return !stateEnum[state]?.operation?.[type];
+    // 表格操作列按钮
+    getListTableColBtnModel() {
+      return (row) => {
+        return listTableColBtnModel(this, row);
       };
     },
   },
@@ -100,33 +70,6 @@ export default {
       this.$router.push({
         name: "Sa_SaleReturn_Detail_list",
       });
-    },
-    //删除单据
-    deleteOrder(id) {
-      this.tableObj.api.del({ BillIds: id }).then((data) => {
-        this.tableObj.getData();
-      });
-    },
-    //新增分类
-    add() {
-      this.$router.push({
-        name: "Sa_SaleReturn_Add",
-        params: { type: "add" },
-      });
-    },
-
-    //编辑
-    edit(e) {
-      this.tableObj.api.editLock({ BillId: e.BillId }).then((res) => {
-        this.$router.push({
-          name: "Sa_SaleReturn_Edit",
-          query: { BillId: e.BillId },
-        });
-      });
-    },
-    //批量删除单据
-    del() {
-      this.deleteOrder(this.tableObj.selectData.keys);
     },
   },
 };
