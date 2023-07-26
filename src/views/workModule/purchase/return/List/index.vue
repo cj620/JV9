d24a0b8394561943f5b32afa1450748e08608980
陈祎初 <18530100115@163.com>
2023-07-26 14:51:52 +0800

    feat: 采购退货操作组件

--- src/views/workModule/purchase/return/List/index.vue
+++ src/views/workModule/purchase/return/List/index.vue
@@ -9,48 +9,12 @@
   <PageWrapper :footer="false">
     <!-- 表格 -->
     <JvTable ref="BillTable" :table-obj="tableObj">
+      <!-- operation操作列 -->
       <template #operation="{ row }">
-        <TableAction
-          :actions="[
-            {
-              label: $t('Generality.Ge_Edit'),
-              confirm: edit.bind(null, row),
-              disabled: !(
-                stateEnum[row.State] && stateEnum[row.State].operation.edit
-              ),
-            },
-            {
-              label: $t('Generality.Ge_Delete'),
-              disabled: !(
-                stateEnum[row.State] && stateEnum[row.State].operation.del
-              ),
-              popConfirm: {
-                title: $t('Generality.Ge_DeleteConfirm'),
-                confirm: deleteOrder.bind(null, [row.BillId]),
-              },
-            },
-          ]"
-        />
+        <TableAction :actions="getListTableColBtnModel(row)" />
       </template>
-      <Action
-        size="mini"
-        slot="btn-list"
-        :actions="[
-          {
-            label: $t('Generality.Ge_New'),
-
-            confirm: add.bind(),
-          },
-          {
-            label: $t('Generality.Ge_Delete'),
-            disabled: canIsDel,
-            popConfirm: {
-              title: $t('Generality.Ge_DeleteConfirm'),
-              confirm: del.bind(),
-            },
-          },
-        ]"
-      >
+      <!-- 表格操作行 -->
+      <Action size="mini" slot="btn-list" :actions="getListTableBtnModel">
       </Action>
     </JvTable>
   </PageWrapper>
@@ -58,36 +22,45 @@
 
 <script>
 import { Table } from "./config";
-import { stateEnum } from "@/enum/workModule";
 import { API } from "@/api/workApi/purchase/return";
 import { editLock } from "@/api/basicApi/systemSettings/billEditLock";
 import BillStateTags from "@/components/WorkModule/BillStateTags";
+import {
+  listTableBtnModel,
+  listTableColBtnModel,
+} from "@/jv_doc/utils/system/pagePlugin";
 export default {
   name: "Pu_Return",
   data() {
+    // return {
+    //   stateEnum,
+    //   // 表格数据
+    //   tableObj: {
+    //     type: "",
+    //     data: "",
+    //   },
+    //   transferData: {
+    //     type: "",
+    //     data: "",
+    //   },
+    //   form: {
+    //     SortColumn: "ItemType",
+    //     SortOrder: 4,
+    //   },
+    //   auditForm: {
+    //     Remarks: "",
+    //     BizFormId: "",
+    //     AuditResult: "",
+    //   },
+    //   remarkShow: false,
+    //   dialogFormVisible: false,
+    //   chooseList: [],
+    // };
     return {
-      stateEnum,
-      // 表格数据
-      tableObj: {
-        type: "",
-        data: "",
-      },
-      transferData: {
-        type: "",
-        data: "",
-      },
-      form: {
-        SortColumn: "ItemType",
-        SortOrder: 4,
-      },
-      auditForm: {
-        Remarks: "",
-        BizFormId: "",
-        AuditResult: "",
-      },
-      remarkShow: false,
-      dialogFormVisible: false,
-      chooseList: [],
+      tableObj: {},
+      // 编辑路由
+      EditRoute: "Pu_Return_Edit",
+      AddRoute: "Pu_Return_Add",
     };
   },
   methods: {
@@ -137,6 +110,22 @@ export default {
       return datas.some((item) => {
         return !["Rejected", "Unsubmitted"].includes(item.State);
       });
+    },
+    // 表格操作模块
+    getListTableBtnModel() {
+      return [
+        ...listTableBtnModel(this),
+        {
+          label: i18n.t("project.Pro_CheckDetails"),
+          confirm: this.toDetailsList,
+        },
+      ];
+    },
+    // 表格操作列按钮
+    getListTableColBtnModel() {
+      return (row) => {
+        return listTableColBtnModel(this, row);
+      };
     },
   },
   components: {
