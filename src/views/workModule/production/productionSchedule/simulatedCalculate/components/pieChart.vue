8410be9969f36019c3827ea802b4bc6664c8c177
陈祎初 <18530100115@163.com>
2023-07-31 15:23:00 +0800

    feat: 模拟计算模拟排程框架

---
+++ src/views/workModule/production/productionSchedule/simulatedCalculate/components/pieChart.vue
@@ -0,0 +1,98 @@
+<!--饼图-->
+<template>
+  <div>
+    <div :id="id" :style="{ height: '100%', minHeight: height + 'px' }"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "pieChart",
+  props: {
+    height: {
+      type: Number,
+      default() {
+        return 300;
+      },
+    },
+    id: {
+      type: String,
+      default() {
+        return null;
+      },
+    },
+    data: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  async mounted() {
+    await this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById(this.id));
+      console.log("myCgart::: ", myChart);
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          // orient: "vertical",
+          // left: "left",
+          top: "5%",
+          left: "center",
+        },
+        series: [
+          {
+            // name: "Access From",
+            type: "pie",
+            radius: ["40%", "70%"],
+            avoidLabelOverlap: false,
+            data: this.data,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: "#fff",
+              borderWidth: 2,
+            },
+            label: {
+              // formatter: "{b}: {c}",
+              show: false,
+              position: "center",
+            },
+            // emphasis: {
+            //   // itemStyle: {
+            //   //   shadowBlur: 10,
+            //   //   shadowOffsetX: 0,
+            //   //   shadowColor: "rgba(0, 0, 0, 0.5)",
+            //   // },
+            //   label: {
+            //     show: true,
+            //     fontSize: 15,
+            //     fontWeight: "bold",
+            //   },
+            // },
+            labelLine: {
+              show: false,
+            },
+          },
+        ],
+      });
+      window.addEventListener("resize", function () {
+        myChart.resize();
+      });
+      myChart.resize();
+      this.$nextTick(() => {
+        myChart.resize(); // 图表自适应
+      });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/workModule/production/productionSchedule/simulatedCalculate/index.vue
+++ src/views/workModule/production/productionSchedule/simulatedCalculate/index.vue
@@ -1,7 +1,7 @@
 <template>
   <PageWrapper ref="page" :footer="false">
-    <div>
-      <el-form>
+    <div class="simulatedCalculate-page">
+      <el-form size="mini">
         <el-form-item label="排程算法选择">
           <el-select
             v-model="value"
@@ -19,20 +19,142 @@
             >模拟计算</el-button
           >
         </el-form-item>
+        <el-tabs type="border-card">
+          <el-tab-pane label="模拟排程">
+            <el-row :gutter="20">
+              <!-- 经典算法 -->
+              <el-col :span="12" class="simulatedCalculate-page-chartwrapper">
+                <div class="chart-top-title">经典算法</div>
+                <div class="simulatedCalculate-page-chart">
+                  <!-- <div class="chart-description">
+                    共有8个普通生产任务单延迟，共延迟128个小时。
+                  </div> -->
+                  <div class="chart-description">
+                    当前共有18个工单正在生产,7个工单待生产。
+                  </div>
+                  <PieChart
+                    id="PieChart1"
+                    :data="AbnormalProportion"
+                  ></PieChart>
+                </div>
+              </el-col>
+              <!-- CR值排程 -->
+              <el-col :span="12" class="simulatedCalculate-page-chartwrapper">
+                <div class="chart-top-title">CR值排程</div>
+                <div class="simulatedCalculate-page-chart">
+                  <div class="chart-description">
+                    共有8个普通生产任务单延迟，共延迟128个小时。
+                  </div>
+                  <PieChart
+                    id="PieChart2"
+                    :data="AbnormalProportion"
+                  ></PieChart>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <!-- 最短工期 -->
+              <el-col :span="12" class="simulatedCalculate-page-chartwrapper">
+                <div class="chart-bottom-title">最短工期</div>
+                <div
+                  class="simulatedCalculate-page-chart"
+                  v-if="AbnormalProportion.length > 0"
+                >
+                  <div class="chart-description">
+                    共有8个普通生产任务单延迟，共延迟128个小时。
+                  </div>
+                  <PieChart
+                    id="PieChart3"
+                    :data="AbnormalProportion"
+                  ></PieChart>
+                </div>
+                <div class="simulatedCalculate-page-chart" v-else>
+                  <div class="chart-description">
+                    当前共有18个工单正在生产,7个工单待生产。
+                  </div>
+                  <PieChart
+                    id="PieChart3"
+                    :data="AbnormalProportion"
+                  ></PieChart>
+                </div>
+              </el-col>
+              <!-- 最早交货期 -->
+              <el-col :span="12" class="simulatedCalculate-page-chartwrapper">
+                <div class="chart-bottom-title">最早交货期</div>
+                <div class="simulatedCalculate-page-chart">
+                  <div class="chart-description">
+                    共有8个普通生产任务单延迟，共延迟128个小时。
+                  </div>
+                  <PieChart
+                    id="PieChart4"
+                    :data="AbnormalProportion"
+                  ></PieChart>
+                </div>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="经典算法">经典算法</el-tab-pane>
+          <el-tab-pane label="CR值排程">CR值排程</el-tab-pane>
+          <el-tab-pane label="最短工期">最短工期</el-tab-pane>
+          <el-tab-pane label="最早交货期">最早交货期</el-tab-pane>
+        </el-tabs>
       </el-form>
     </div>
   </PageWrapper>
 </template>
 
 <script>
+import PieChart from "./components/pieChart";
 export default {
   name: "index",
+  components: {
+    PieChart,
+  },
   data() {
     return {
       value: [],
+      AbnormalProportion: [
+        { value: 0, name: "正常任务单", itemStyle: { color: "#00FF00" } },
+        { value: 0, name: "延迟任务单", itemStyle: { color: "red" } },
+      ],
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-form-item {
+  margin-bottom: 14px;
+}
+.simulatedCalculate-page {
+  height: 100%;
+  background-color: #ffffff;
+  padding: 10px;
+  .simulatedCalculate-page-chartwrapper {
+    padding: 0 20px;
+    height: 40%;
+    .simulatedCalculate-page-chart {
+      border: 2px solid gainsboro;
+      margin: 0 10% 0 10%;
+    }
+  }
+}
+.chart-top-title {
+  margin-bottom: 5px;
+  font-size: 16px;
+  font-weight: 500;
+  margin: 0 10% 5px 10%;
+}
+.chart-bottom-title {
+  margin: 10px 0 5px 0;
+  font-size: 16px;
+  font-weight: 500;
+  margin: 10px 10% 5px 10%;
+}
+.chart-description {
+  font-size: 14px;
+  font-weight: 500;
+  margin-top: 12px;
+  text-align: center;
+}
+</style>
