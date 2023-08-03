95bbe2640099fbc2d1d6ab8389bda3f9c2c8347d
C <cj6209577@sina.com>
2023-08-03 16:04:19 +0800

    feat: 处理已读未读

--- src/layout/components/Sidebar/NotifyItem.vue
+++ src/layout/components/Sidebar/NotifyItem.vue
@@ -1,13 +1,17 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-27 09:45:11
- * @LastEditTime: 2023-08-02 17:28:56
+ * @LastEditTime: 2023-08-03 15:35:55
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \V9_Dev\src\layout\components\Sidebar\Logo.vue
 -->
 <template>
-  <div class="notify-item">
+  <div
+    class="notify-item"
+    :class="cdata.IsRead ? 'isRead' : ''"
+    @click="toDetail"
+  >
     <div
       style="
         display: flex;
@@ -39,6 +43,7 @@
 <script>
 import { imgUrlPlugin } from "@/jv_doc/utils/system/index.js";
 import { formatTime } from "@/utils/index";
+import { mark_as_read } from "@/api/basicApi/systemSettings/notification";
 export default {
   name: "NotifyItem",
   props: {
@@ -53,6 +58,25 @@ export default {
   methods: {
     imgUrlPlugin,
     formatTime,
+    async toDetail() {
+      await mark_as_read({
+        Id: this.cdata.Id,
+        NotificationType: this.cdata.Type,
+        OneClickRead: false,
+      });
+      // DynamicDataValue
+      const { BillId, BillKey } = this.cdata.DynamicDataValue;
+      // console.log(BillId, BillKey);
+      if (BillId && BillKey) {
+        const str = `${BillKey}_Detail`;
+        this.$router.push({
+          name: str,
+          query: {
+            BillId: BillId,
+          },
+        });
+      }
+    },
   },
 };
 </script>
@@ -62,9 +86,14 @@ export default {
   /* height: 30px; */
   min-height: 50px;
   padding: 10px 5px 10px 5px;
-  border-bottom: 1px solid #e2e2e2;
+  /* border-bottom: 1px solid #e2e2e2; */
   &:hover {
-    background-color: #ccc;
+    background-color: #eee;
   }
+  cursor: pointer;
+}
+.isRead {
+  /* filter: grayscale(100%); */
+  color: #ccc;
 }
 </style>
