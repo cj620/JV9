ef796aca91c3ef1787e68500bc0faacbd00c4770
陈祎初 <18530100115@163.com>
2023-08-11 09:36:53 +0800

    feat: 删除发布完成弹窗，修改弹窗不关闭bug

--- src/views/workModule/production/productionSchedule/List/index.vue
+++ src/views/workModule/production/productionSchedule/List/index.vue
@@ -184,19 +184,7 @@
         ApsVersionNo
       }}
     </jv-dialog>
-    <!-- 版本号弹窗 -->
-    <jv-dialog
-      v-if="versionDialogFormVisible"
-      :IsShowCancelFooterBtn="false"
-      :title="$t('Generality.Ge_Remind')"
-      :visible.sync="versionDialogFormVisible"
-      width="30%"
-      @confirm="closeVersion"
-    >
-      排程结果已发布，版本号：{{ ApsVersionNo }}
-    </jv-dialog>
-
-    <!-- 发布排程结果弹窗 -->
+    <!-- 发布按钮提醒 -->
     <JvDialog
       :title="$t('Generality.Ge_Remind')"
       :visible.sync="SchedulingResultsVisible"
@@ -241,7 +229,6 @@ export default {
       ApsVersionNo: "",
       calculateTimeDialogFormVisible: false,
       releaseDialogFormVisible: false,
-      versionDialogFormVisible: false,
       SchedulingResultsVisible: false, // 发布排程结果弹窗
       // 路由跳转前是否提醒发布
       needOpen: false,
@@ -385,8 +372,9 @@ export default {
     release() {
       do_publish().then(() => {
         this.releaseDialogFormVisible = false;
-        this.versionDialogFormVisible = true;
+        this.SchedulingResultsVisible = false
         this.needOpen = false;
+        this.tableObj.getData();
       });
     },
     // 发布弹窗取消
@@ -398,11 +386,6 @@ export default {
         : console.log("无需跳转");
       this.toRouteName = null;
     },
-    // 关闭版本号弹窗
-    closeVersion() {
-      this.versionDialogFormVisible = false;
-    },
-    // ===========================================
     // 切换时间
     setGanttZoom(val) {
       this.$refs.CustomGantt.setGanttZoom(val);
