00ed991cbca4ba43934d72970ed0922c41d47929
陈旭 <1587493007@qq.com>
2023-08-22 09:25:33 +0800

    修改项目进度页面样式

--- src/i18n/lang/en/index.js
+++ src/i18n/lang/en/index.js
@@ -341,6 +341,8 @@ const en = {
   },
   //通用
   Generality: {
+    Ge_Chart: "Chart",
+    Ge_Gantt: "Gantt",
     Ge_GanttShow: "Gantt Chart Shows",
     Ge_TabularShow: "Tabular Display",
     Ge_New: "New",
