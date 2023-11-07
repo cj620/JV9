c92804504d9dc1cb971a70a59754c9ec5ae1cfb5
陈旭 <1587493007@qq.com>
2023-11-07 14:00:56 +0800

    甘特图菜单功能

--- src/views/workModule/production/NewProductionSchedule/GanttChart/formConfig.js
+++ src/views/workModule/production/NewProductionSchedule/GanttChart/formConfig.js
@@ -1,5 +1,17 @@
 export const formSchema = [
   {
+    prop: "PlanDevice",
+    custom: true,
+    label: '机台',
+    rules: [
+      {
+        required: true,
+        message: i18n.t("Generality.Ge_PleaseEnter"),
+        trigger: ["change", "blur"],
+      },
+    ],
+  },
+  {
     prop: "PlanStart",
     cpn: "SingleDateTime",
     label: i18n.t("Generality.Ge_PlanStart"),
