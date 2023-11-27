5b7f6f20d32825ce24eefeb5268ce9e0c0ef8a8a
陈祎初 <18530100115@163.com>
2023-11-27 16:45:27 +0800

    feat: 保养方案bug

--- src/views/workModule/equipment/accessoryCheckOut/List/index.vue
+++ src/views/workModule/equipment/accessoryCheckOut/List/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>1</div>
+  <PageWrapper :footer="false">
+    <JvTable ref="BillTable" :table-obj="tableObj">
+
+    </JvTable>
+  </PageWrapper>
 </template>
 <script>
+export default {
+  name: "As_AccessoryCheckOut",
+  data() {
+    return {
+      tableObj: {},
+    }
+  }
+}
 
 </script>
