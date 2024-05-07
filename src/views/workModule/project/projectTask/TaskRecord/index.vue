11b692af70ab4cddfc987a8e3e21cae9a1754122
陈祎初 <18530100115@163.com>
2024-05-07 14:42:07 +0800

    feat: 项目任务记录调整时间

--- src/views/workModule/project/projectTask/TaskRecord/index.vue
+++ src/views/workModule/project/projectTask/TaskRecord/index.vue
@@ -17,17 +17,35 @@
       </template>
       <!-- operation操作列 -->
       <template #operation="{ row }">
-        <TableAction :actions="getListTableColBtnModel(row)" />
+        <TableAction
+          :actions="[
+            {
+              label: $t('Generality.Ge_Edit'),
+              confirm: editRecord.bind(null, row)
+            },
+          ]"
+        />
       </template>
     </JvTable>
+    <JvDialog
+      :title="$t('Generality.Ge_Edit')"
+      :visible.sync="editRecordDialogVisible"
+      v-if="editRecordDialogVisible"
+      destroy-on-close
+      @confirm="confirmToEdit"
+      width="30%"
+    >
+      <JvForm :formObj="editForm"></JvForm>
+    </JvDialog>
   </PageWrapper>
 </template>
 <script>
 // 引入表格类
-import { Table } from "./config";
-// 单据状态组件
+import { Table, formSchema1 } from "./config";
+import { Form } from "~/class/form";
 import { taskTypeEnum } from "@/enum/workModule";
-import { listTableColBtnModel } from "@/jv_doc/utils/system/pagePlugin";
+import { update_record_actual_end } from "@/api/workApi/project/projectTask"
+import { timeFormat } from "~/utils/time";
 export default {
   // 页面的标识
   // name: "Pm_ProjectTask_Record",
@@ -35,6 +53,8 @@ export default {
     return {
       // 表格实例
       tableObj: {},
+      editForm: {},
+      editRecordDialogVisible: false,
       taskTypeEnum,
     };
   },
@@ -48,13 +68,50 @@ export default {
       this.tableObj.formObj.form.TaskType = this.$route.query.TaskType;
       this.tableObj.getData();
     },
-  },
-  computed: {
-    getListTableColBtnModel() {
-      return (row) => {
-        return listTableColBtnModel(this, row);
-      };
+    editRecord(row) {
+      this.editForm = new Form({
+        formSchema: formSchema1,
+        labelPosition: "top",
+        baseColProps: {
+          span: 24,
+        },
+        labelWidth: "80px",
+      })
+      this.editRecordDialogVisible = true;
+      this.editForm.form.EndDate = row.ActualEnd;
+      this.editForm.form.StartDate = row.ActualStart;
+      this.editForm.form.Progress = row.Progress;
+      this.editForm.form.WorkHour = row.ActualTime;
+      this.editForm.form.Id = row.Id;
     },
+    confirmToEdit() {
+      this.editForm.validate((valid) => {
+        if (valid) {
+          this.editForm.form.StartDate =timeFormat(this.editForm.form.StartDate,'yyyy-MM-dd hh:mm:ss')
+          this.editForm.form.EndDate =timeFormat(this.editForm.form.EndDate,'yyyy-MM-dd hh:mm:ss')
+          update_record_actual_end({
+            Id: this.editForm.form.Id,
+            ActualStart: this.editForm.form.StartDate,
+            ActualEnd: this.editForm.form.EndDate,
+            ActualTime: this.editForm.form.WorkHour,
+            Progress: this.editForm.form.Progress,
+          }).then(() => {
+            this.tableObj.getData();
+            this.editRecordDialogVisible = false;
+          })
+        }
+      })
+    }
   },
+  watch:{
+    'editForm.form.WorkHour':{
+      handler(n,o){
+        if(n){
+          // n*60*60*1000
+          this.editForm.form.StartDate = new Date(new Date(this.editForm.form.EndDate).getTime() - n * 60 * 60 * 1000);
+        }
+      }
+    }
+  }
 };
 </script>
