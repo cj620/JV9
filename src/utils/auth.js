54154b66e1c63e8fedec5a9553f0c4110cc540c4
C <cj6209577@sina.com>
2024-05-13 13:09:42 +0800

    feat: token转储

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,28 @@
-import Cookies from 'js-cookie'
+// import Cookies from "js-cookie";
 
-const TokenKey = 'Admin-Token'
+// const TokenKey = "Admin-Token";
+
+// export function getToken() {
+//   return Cookies.get(TokenKey);
+// }
+
+// export function setToken(token) {
+//   return Cookies.set(TokenKey, token);
+// }
+
+// export function removeToken() {
+//   return Cookies.remove(TokenKey);
+// }
+const TokenKey = "Admin-Token";
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey);
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token);
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey);
 }
