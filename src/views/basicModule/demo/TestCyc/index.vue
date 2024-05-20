961290a25a55afe5c9dc0510830e94a6757bbe98
陈祎初 <18530100115@163.com>
2024-05-20 17:27:49 +0800

    feat: 测试

---
+++ src/views/basicModule/demo/TestCyc/index.vue
@@ -0,0 +1,416 @@
+<template>
+  <PageWrapper ref="page" :footer="false">
+    <div class="weekly-task-page">
+      <table>
+        <tr>
+          <th rowspan="2">需求日期</th>
+          <th colspan="4">魏鹏程</th>
+          <th colspan="4">王威付</th>
+          <th colspan="4">张淮滨</th>
+          <th colspan="4">张嘉达</th>
+        </tr>
+        <tr>
+<!--          <th>需求日期</th>-->
+          <th>模号</th>
+          <th>内容</th>
+          <th>预估工时</th>
+          <th>是否完成</th>
+          <th>模号</th>
+          <th>内容</th>
+          <th>预估工时</th>
+          <th>是否完成</th>
+          <th>模号</th>
+          <th>内容</th>
+          <th>预估工时</th>
+          <th>是否完成</th>
+          <th>模号</th>
+          <th>内容</th>
+          <th>预估工时</th>
+          <th>是否完成</th>
+        </tr>
+        <tr>
+          <td rowspan="3" class="text-center">
+            周六<br>5月13日
+          </td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周六</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周六</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+          <td rowspan="3" class="text-center">
+            周日<br>5月14日
+          </td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周日</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周日</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+          <td rowspan="3" class="text-center">
+            周一<br>5月15日
+          </td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周一</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+        <tr>
+<!--          <td>周一</td>-->
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+          <td>FAC-001</td>
+          <td>新模DFM</td>
+          <td>3</td>
+          <td>是</td>
+        </tr>
+      </table>
+    </div>
+  </PageWrapper>
+</template>
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      userData: [
+        {
+          user: "魏鹏程",
+          data: [
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+          ],
+        },
+        {
+          user: "王威付",
+          data: [
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+          ],
+        },
+        {
+          user: "张淮滨",
+          data: [
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+          ],
+        },
+        {
+          user: "张嘉达",
+          data: [
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+            [
+              { ToolingNo: "FAC-001", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-002", WorkContent: "新模DFM", PlanTime: "3", isCompleted: true  },
+              { ToolingNo: "FAC-003", WorkContent: "新模DFM", PlanTime: "2", isCompleted: true  },
+            ],
+          ],
+        },
+      ],
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.weekly-task-page {
+  height: 100%;
+  background-color: #ffffff;
+  padding: 10px 15px 10px 15px;
+  table {
+    border: 1px solid black;
+    margin: 0 auto;
+    border-collapse: collapse;
+    border-spacing: 5px 5px;
+    width: 100%;
+    th {
+      padding: 10px;
+      border-collapse: collapse;
+      background: rgb(237, 237, 237);
+      border:1px solid rgb(241, 242, 247);
+      color: rgb(79, 79, 79);
+      font-weight: 500;
+      font-size: 14px;
+    }
+    td {
+      padding: 10px;
+      border-collapse: collapse;
+      border: 1px solid #dfe6ec;
+      font-size: 14px;
+      color: #606266;
+    }
+    .text-center {
+      vertical-align: middle;
+      text-align: center;
+    }
+  }
+}
+</style>
