189bc47367d1d0eedc75c04f0900e47c8c9dfdd9
陈祎初 <18530100115@163.com>
2024-05-22 16:53:31 +0800

    feat: 项目任务工序工作内容添加字段

--- src/views/workModule/project/projectTask/Detail/config.js
+++ src/views/workModule/project/projectTask/Detail/config.js
@@ -7,24 +7,67 @@
  * @FilePath: \V9_Dev\src\views\workModule\project\projectManage\mouldDetail\config.js
  */
 import { Table as BaseTable } from "@/jv_doc/class/table";
-import { m_tableConfig as tableConfig } from "../Add/editConfig";
-import { taskTypeEnum, enumToList } from "@/enum/workModule";
-const newTableCongfig = [...tableConfig];
-newTableCongfig.splice(tableConfig.length - 1, 0, {
-  prop: "Progress",
-  label: i18n.t("Generality.Ge_Schedule"),
-  custom: true,
-});
+
 export class Table extends BaseTable {
   constructor() {
     super({
-      tableSchema: newTableCongfig,
+      tableSchema: tableConfig,
       tableHeaderShow: false,
       pagination: false,
       height: null,
     });
   }
 }
+
+export const tableConfig = [
+  /*工序*/
+  {
+    prop: "Process",
+    label: i18n.t("Generality.Ge_Process"),
+    width: '180px',
+  },
+  /*工作内容*/
+  {
+    prop: "ProcessContent",
+    label: i18n.t("Generality.Ge_WorkContent"),
+  },
+  /*负责人*/
+  {
+    prop: "Worker",
+    width: "120px",
+    label: i18n.t("project.Pro_Worker"),
+  },
+  /*计划工时*/
+  {
+    prop: "PlanTime",
+    label: i18n.t("Generality.Ge_PlanTime"),
+    width: "120px",
+  },
+  /*计划开始*/
+  {
+    prop: "PlanStart",
+    label: i18n.t("Generality.Ge_PlanStart"),
+    filter: "date",
+    width: "150px",
+  },
+  /*计划结束*/
+  {
+    prop: "PlanEnd",
+    label: i18n.t("Generality.Ge_PlanEnd"),
+    filter: "date",
+    width: "150px",
+  },
+  {
+    prop: "Progress",
+    label: i18n.t("Generality.Ge_Schedule"),
+    custom: true,
+  },
+  /*备注*/
+  {
+    prop: "Remarks",
+    label: i18n.t("Generality.Ge_Remarks"),
+  },
+]
 
 export const detailConfig = [
   //模具编号
