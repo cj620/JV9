04ce5de0b72333415086292c938d8e46b17dc83f
C <cj6209577@sina.com>
2024-06-07 13:24:10 +0800

    feat: 设计工时汇总

--- src/router/workModule/design.js
+++ src/router/workModule/design.js
@@ -1,7 +1,7 @@
 /*
  * @Author: H.
  * @Date: 2021-11-09 09:08:00
- * @LastEditTime: 2022-01-14 11:32:31
+ * @LastEditTime: 2024-06-07 11:12:18
  * @Description:设计模块
  */
 
@@ -10,125 +10,144 @@ import Layout from "@/layout";
 
 // 设计模块
 const design = [
-	// 设计
-	{
-		key: "De_Design",
-		Parent: null,
-		name: "Design",
-		path: "design",
-		component: Layout,
-		title: i18n.t("menu.De_Design"),
-		icon: "design",
-	},
-	// 设计任务
-	{
-		key: "De_DesignTask",
-		component: () => import("@/views/workModule/design/designTask/List"),
-		Parent: "De_Design",
-		name: "DesignTask",
-		path: "designTask",
-		title: i18n.t("menu.De_DesignTask"),
-	},
-	// 设计任务记录
-	{
-		key: "De_DesignTask_Record",
-		component: () => import("@/views/workModule/design/designTask/TaskRecord"),
-		Parent: "De_Design",
-		name: "DesignTask_Record",
-		path: "designTask_Record",
-		title: i18n.t("design.De_DesignTaskRecord"),
-		hidden: true,
-	},
-	// 设计任务列表
-	{
-		key: "De_DesignTaskList",
-		component: () =>
-			import("@/views/workModule/design/designTask/DesignTaskList"),
-		Parent: "De_Design",
-		name: "De_DesignTaskList",
-		path: "De_DesignTaskList",
-		title: i18n.t("menu.De_DesignTaskList"),
-	},
-	// 模具BOM
-	{
-		key: "De_ToolingBOM",
-		component: () => import("@/views/workModule/design/toolingBOM/List"),
-		Parent: "De_Design",
-		name: "ToolingBOM",
-		path: "toolingBOM",
-		title: i18n.t("menu.De_ToolingBOM"),
-	},
-	// 工艺设计
-	{
-		key: "De_CraftDesign",
-		component: () => import("@/views/workModule/design/toolingBOM/craftDesign"),
-		Parent: "De_Design",
-		name: "CraftDesign",
-		path: "craftDesign",
-		title: i18n.t("program.Pr_ProcessPlanning"),
-		hidden: true,
-	},
+  // 设计
+  {
+    key: "De_Design",
+    Parent: null,
+    name: "Design",
+    path: "design",
+    component: Layout,
+    title: i18n.t("menu.De_Design"),
+    icon: "design",
+  },
+  // 设计任务
+  {
+    key: "De_DesignTask",
+    component: () => import("@/views/workModule/design/designTask/List"),
+    Parent: "De_Design",
+    name: "DesignTask",
+    path: "designTask",
+    title: i18n.t("menu.De_DesignTask"),
+  },
+  // 设计任务记录
+  {
+    key: "De_DesignTask_Record",
+    component: () => import("@/views/workModule/design/designTask/TaskRecord"),
+    Parent: "De_Design",
+    name: "DesignTask_Record",
+    path: "designTask_Record",
+    title: i18n.t("design.De_DesignTaskRecord"),
+    hidden: true,
+  },
+  // 设计任务列表
+  {
+    key: "De_DesignTaskList",
+    component: () =>
+      import("@/views/workModule/design/designTask/DesignTaskList"),
+    Parent: "De_Design",
+    name: "De_DesignTaskList",
+    path: "De_DesignTaskList",
+    title: i18n.t("menu.De_DesignTaskList"),
+  },
+  {
+    key: "De_DesignTask_PersonnelHoursSummary",
+    component: () => import("@/views/workModule/design/designTask/HourSummary"),
+    Parent: "Pr_Report",
+    name: "De_DesignTask_PersonnelHoursSummary",
+    path: "De_DesignTask_PersonnelHoursSummary",
+    title: "设计工时汇总",
+  },
+  // 模具BOM
+  {
+    key: "De_ToolingBOM",
+    component: () => import("@/views/workModule/design/toolingBOM/List"),
+    Parent: "De_Design",
+    name: "ToolingBOM",
+    path: "toolingBOM",
+    title: i18n.t("menu.De_ToolingBOM"),
+  },
+  // 工艺设计
+  {
+    key: "De_CraftDesign",
+    component: () => import("@/views/workModule/design/toolingBOM/craftDesign"),
+    Parent: "De_Design",
+    name: "CraftDesign",
+    path: "craftDesign",
+    title: i18n.t("program.Pr_ProcessPlanning"),
+    hidden: true,
+  },
 
-
-	// 物料需求
-	{
-		key: "De_MaterialRequirement",
-		component: () => import("@/views/workModule/design/itemsDemand/List"),
-		Parent: "De_Design",
-		name: "ItemsDemand",
-		path: "itemsDemand",
-		title: i18n.t("menu.De_MaterialRequirement"),
-	},
-	// 新增物料需求
-	{
-		key: "De_MaterialRequirement_Add",
-		component: () => import("@/views/workModule/design/itemsDemand/Add"),
-		Parent: "De_Design",
-		name: "De_MaterialRequirement_Add",
-		path: "De_MaterialRequirement_Add",
-		title: i18n.t("Generality.Ge_New") + "  " + i18n.t("menu.De_MaterialRequirement"),
-		hidden: true,
-	},
-	// 编辑物料需求
-	{
-		key: "De_MaterialRequirementuirement_Edit",
-		component: () => import("@/views/workModule/design/itemsDemand/Edit"),
-		Parent: "De_Design",
-		name: "De_MaterialRequirement_Edit",
-		path: "De_MaterialRequirement_Edit",
-		title: i18n.t("Generality.Ge_Edit") + "  " + i18n.t("menu.De_MaterialRequirement"),
-		hidden: true,
-	},
-	// 物料需求详情
-	{
-		key: "De_MaterialRequirement_Detail",
-		component: () => import("@/views/workModule/design/itemsDemand/Details"),
-		Parent: "De_Design",
-		name: "De_MaterialRequirement_Detail",
-		path: "De_MaterialRequirement_Detail",
-		title:
-			i18n.t("menu.De_MaterialRequirement") + "  " + i18n.t("Generality.Ge_Details"),
-		hidden: true,
-	},
-	//物料需求明细
-	{
-		key: "De_MaterialRequirement_Detail_list",
-		component: () => import("@/views/workModule/design/itemsDemand/DetailsList"),
-		Parent: "De_Design",
-		name: "De_MaterialRequirement_Detail_list",
-		path: "De_MaterialRequirement_Detail_list",
-		title: i18n.t("menu.De_MaterialRequirement") + '   ' + i18n.t('Generality.Ge_Details1'),
-		hidden: true,
-	},
-	// 生产需求
-	{
-		key: "De_ProductionDemand",
-		component: () => import("@/views/workModule/design/productionDemand/List"),
-		Parent: "De_Design",
-		name: "ProductionDemand",
-		path: "productionDemand",
-		title: i18n.t("menu.De_ProductionDemand"),
-	},
+  // 物料需求
+  {
+    key: "De_MaterialRequirement",
+    component: () => import("@/views/workModule/design/itemsDemand/List"),
+    Parent: "De_Design",
+    name: "ItemsDemand",
+    path: "itemsDemand",
+    title: i18n.t("menu.De_MaterialRequirement"),
+  },
+  // 新增物料需求
+  {
+    key: "De_MaterialRequirement_Add",
+    component: () => import("@/views/workModule/design/itemsDemand/Add"),
+    Parent: "De_Design",
+    name: "De_MaterialRequirement_Add",
+    path: "De_MaterialRequirement_Add",
+    title:
+      i18n.t("Generality.Ge_New") +
+      "  " +
+      i18n.t("menu.De_MaterialRequirement"),
+    hidden: true,
+  },
+  // 编辑物料需求
+  {
+    key: "De_MaterialRequirementuirement_Edit",
+    component: () => import("@/views/workModule/design/itemsDemand/Edit"),
+    Parent: "De_Design",
+    name: "De_MaterialRequirement_Edit",
+    path: "De_MaterialRequirement_Edit",
+    title:
+      i18n.t("Generality.Ge_Edit") +
+      "  " +
+      i18n.t("menu.De_MaterialRequirement"),
+    hidden: true,
+  },
+  // 物料需求详情
+  {
+    key: "De_MaterialRequirement_Detail",
+    component: () => import("@/views/workModule/design/itemsDemand/Details"),
+    Parent: "De_Design",
+    name: "De_MaterialRequirement_Detail",
+    path: "De_MaterialRequirement_Detail",
+    title:
+      i18n.t("menu.De_MaterialRequirement") +
+      "  " +
+      i18n.t("Generality.Ge_Details"),
+    hidden: true,
+  },
+  //物料需求明细
+  {
+    key: "De_MaterialRequirement_Detail_list",
+    component: () =>
+      import("@/views/workModule/design/itemsDemand/DetailsList"),
+    Parent: "De_Design",
+    name: "De_MaterialRequirement_Detail_list",
+    path: "De_MaterialRequirement_Detail_list",
+    title:
+      i18n.t("menu.De_MaterialRequirement") +
+      "   " +
+      i18n.t("Generality.Ge_Details1"),
+    hidden: true,
+  },
+  // 生产需求
+  {
+    key: "De_ProductionDemand",
+    component: () => import("@/views/workModule/design/productionDemand/List"),
+    Parent: "De_Design",
+    name: "ProductionDemand",
+    path: "productionDemand",
+    title: i18n.t("menu.De_ProductionDemand"),
+  },
   // 设计任务明细点检
   {
     key: "De_DesignTaskItemInspection",
@@ -141,7 +160,8 @@ const design = [
   // 分发设计任务
   {
     key: "De_DesignDistributeTask",
-    component: () => import("@/views/workModule/design/designTask/DistributeTask"),
+    component: () =>
+      import("@/views/workModule/design/designTask/DistributeTask"),
     Parent: "De_Design",
     name: "De_DesignDistributeTask",
     path: "De_DesignDistributeTask",
@@ -150,7 +170,8 @@ const design = [
   // 延期设计任务
   {
     key: "De_DesignDelayedTasks",
-    component: () => import("@/views/workModule/design/designTask/DesignDelayedTasks"),
+    component: () =>
+      import("@/views/workModule/design/designTask/DesignDelayedTasks"),
     Parent: "De_Design",
     name: "De_DesignDelayedTasks",
     path: "De_DesignDelayedTasks",
@@ -164,7 +185,7 @@ const design = [
     name: "De_DesignWeeklyDashboard",
     path: "De_DesignWeeklyDashboard",
     title: i18n.t("menu.De_DesignWeeklyDashboard"),
-  }
+  },
 ];
 
 export default design;
---
+++ src/views/workModule/design/designTask/HourSummary/index.vue
@@ -0,0 +1,75 @@
+<!--
+ * @Author: C.
+ * @Date: 2022-10-17 17:15:34
+-->
+<template>
+  <PageWrapper :footer="false">
+    <JvTable ref="BillTable" :table-obj="tableObj">
+      <template #operation="{ row }">
+        <TableAction
+          :actions="[
+            {
+              label: $t('project.Pro_CheckDetails'),
+              confirm: checkDetail.bind(null, row),
+            },
+          ]"
+        ></TableAction>
+      </template>
+    </JvTable>
+    <JvDialog
+      :title="$t('project.Pro_CheckDetails')"
+      :visible.sync="dialogVisible"
+      width="90%"
+      @confirm="dialogVisible = false"
+    >
+      <JvTable :table-obj="workRecordDetailTableObj"> </JvTable>
+    </JvDialog>
+  </PageWrapper>
+</template>
+
+<script>
+import { Table, WorkRecordDetailTable } from "./config";
+import TableAction from "@/jv_doc/cpn/JvTable/cpn/TableAction/index.vue";
+import JvTable from "@/jv_doc/cpn/JvTable/index.vue";
+import { getConfigKey } from "@/api/basicApi/systemSettings/sysSettings";
+
+export default {
+  data() {
+    return {
+      tableObj: {},
+      dialogVisible: false,
+      workRecordDetailTableObj: {},
+    };
+  },
+  async created() {
+    this.tableObj = new Table();
+   await this.GetDeptData()
+    this.workRecordDetailTableObj = new WorkRecordDetailTable();
+
+    this.tableObj.getData();
+  },
+  methods: {
+    //获取部门数据
+    async  GetDeptData(){
+      await  getConfigKey({ConfigKey:'DefaultProductionDepartment'}).then(res=>{
+        console.log(res.ConfigValue,res,565656)
+        console.log(JSON.parse(res.ConfigValue),565656)
+
+        this.tableObj.formObj.form.DepartmentNames=JSON.parse(res.ConfigValue)
+      })
+    },
+    checkDetail(row) {
+      this.workRecordDetailTableObj.formObj.setForm({
+        StartDate: this.tableObj.formObj.form.StartDate,
+        EndDate: this.tableObj.formObj.form.EndDate,
+        Worker: row.Worker,
+      });
+      this.workRecordDetailTableObj.getData();
+      this.dialogVisible = true;
+    },
+  },
+  components: { TableAction, JvTable },
+};
+</script>
+
+<style scoped></style>
