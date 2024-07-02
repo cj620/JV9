ff4f07e524d9099c42477ad5570b5d90e4b664d9
C <cj6209577@sina.com>
2024-07-02 15:01:58 +0800

    feat: 销售发货

--- src/views/workModule/sale/saleDelivery/components/formConfig.js
+++ src/views/workModule/sale/saleDelivery/components/formConfig.js
@@ -1,7 +1,7 @@
 /*
  * @Author: C.
  * @Date: 2021-11-10 13:06:51
- * @LastEditTime: 2022-02-19 10:34:59
+ * @LastEditTime: 2024-07-02 14:59:25
  * @Description: file content
  */
 // import { getAll } from "@/api/basicApi/systemSettings/currency";
@@ -106,6 +106,14 @@ export const formSchema = [
     cpn: "FormInput",
     label: i18n.t("sale.Sa_RelationId"),
   },
+  ///客户地址
+  { label: "客户地址", prop: "Address", cpn: "FormInput" },
+  ///预计开票日期
+  { label: "预计开票日期", prop: "PlanInvoiceDate", cpn: "SingleTime" },
+  ///汇率
+  { label: "汇率", prop: "ExchangeRate", cpn: "FormInput" },
+  ///部门
+  { label: "部门", prop: "Dept", cpn: "FormInput" },
 ];
 export const detailConfig = [
   {
