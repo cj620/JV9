a11eef17674ad652694d96b6de0832ab1bd3eee5
风还会再来吗 <1587493007@qq.com>
2024-07-04 08:40:24 +0800

    采购对账字段对接

---
+++ src/enum/workModule/Purchase/AccountTypeEnum.js
@@ -0,0 +1,14 @@
+export const AccountTypeEnum = {
+  //   合格 OK
+  // NG
+
+  PurchaseStorage: {
+    name: "入库发票",
+    value: "PurchaseStorage",
+  },
+  // NG
+  PurchaseOrder: {
+    name: "订单发票",
+    value: "PurchaseOrder",
+  },
+};
