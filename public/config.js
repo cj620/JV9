75d6d974f3b8e6d27d8072d4b7946bc954395634
C <cj6209577@sina.com>
2024-07-09 11:47:11 +0800

    feat: 图片兼容

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-09-10 09:12:58
- * @LastEditTime: 2024-05-23 13:23:42
+ * @LastEditTime: 2024-07-05 10:04:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-element-admin-master\public\config.js
@@ -9,7 +9,7 @@
 window.global_config = {
   Base_Url: "http://localhost:61823//api",
   // Base_Url: "http://192.168.1.66:9777/api",
-  ImgBase_Url: "http://192.168.1.66:9777/", // 上传图片的地址
+  ImgBase_Url: "http://192.168.1.66:8070/", // 上传图片的地址
   Share_Url: "http://www.jverp.com:8101",
   WebSocket_Url: "ws://www.jverp.com:8021",
   // 税率计算模式  true未国外模式  false为国内模式
