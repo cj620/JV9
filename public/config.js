3cac59ea4449f7f33964fa73373fb310f1be29d8
lxh <208760845@qq.com>
2024-07-24 16:33:50 +0800

    修改图片显示的问题点

--- public/config.js
+++ public/config.js
@@ -8,10 +8,10 @@
  */
 window.global_config = {
   //Base_Url: "http://localhost:61823//api",
-   Base_Url: "http://192.168.1.66:8015/api",
+  Base_Url: "http://192.168.1.66:8015/api",
   ImgBase_Url: "http://192.168.1.66:8015/", // 上传图片的地址
-  Share_Url: "http://www.jverp.com:8101",
-  WebSocket_Url: "ws://www.jverp.com:8021",
+  Share_Url: "http://192.168.1.66:8101",
+  WebSocket_Url: "ws://192.168.1.66:8021",
   // 税率计算模式  true未国外模式  false为国内模式
   ETaxMode: false,
   DashboardRefreshTime: 60000, // 看板刷新时间
