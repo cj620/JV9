9944c8dec2e797012847e81d90f8df74fe10966a
chad陈旭 <1587493007@qq.com>
2024-07-30 17:38:00 +0800

    所有选编程工艺的地方要改成直接选编程那个分类的工艺
    试模任务新增的时候，模具详情图片 要带到试模任务那边

--- src/views/workModule/production/baseData/partProcessTemplate/components/editConfig.js
+++ src/views/workModule/production/baseData/partProcessTemplate/components/editConfig.js
@@ -95,6 +95,9 @@ export const tableConfig = [
       immediate: true,
       keyName: "Process",
       valueName: "Process",
+      fieldFilter: (val) => {
+        return val.ProcessType === 'Program'
+      }
     }
   },
   /*编程预计工时*/
